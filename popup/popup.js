ac39aad15e0b9a1c64266c766ccc39957ef35a39
Daniel Naber <naber@danielnaber.de>
2017-11-24 14:34:01 +0100

    make work (no auth yet)

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,7 @@
 
+//const apiEndpoint = "http://localhost:8000/submitErrorExample";
+const apiEndpoint = "https://languagetoolplus.com/submitErrorExample";
+
 function showText(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {action: 'getText'}, function(response) {
         document.getElementById("origText").value = response;
@@ -17,6 +20,28 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log("no chrome && chrome.tabs");
     }
     document.getElementById("form").addEventListener('submit', function() {
-        self.close();
+        const req = new XMLHttpRequest();
+        req.timeout = 60 * 1000; // milliseconds
+        req.open('POST', apiEndpoint, true);
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        req.onload = function() {
+            if (req.status === 200) {
+                self.close();
+            } else {
+                alert("Sorry, error submitting sentence. Code: " + req.status);
+            }
+        };
+        req.onreadystatechange = function() {
+            //console.log("onreadystatechange", req.status, req.readyState);
+            if (req.readyState == XMLHttpRequest.DONE && req.status == 0) {
+                // not sure why this sometimes on first click...
+                self.close();
+            }
+        };
+        req.send(
+            "sentence=" + encodeURIComponent(document.getElementById("origText").value) +
+            "&correction=" + encodeURIComponent(document.getElementById("correctedText").value) +
+            "&url=" + encodeURIComponent(document.getElementById("url").value)
+        );
     });
 });
